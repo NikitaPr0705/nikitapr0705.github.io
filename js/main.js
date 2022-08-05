860bc0f0a9ee7e1ec9cabd15b11754081de51e8b
Nikita Protsko <44682461+NikitaPr0705@users.noreply.github.com>
2022-08-05 15:35:14 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
 // Custom scripts
 // Мобильное меню бургер
 function burgerMenu() {
-    const burger = document.querySelector('.burger')
-    const menu = document.querySelector('.menu')
-    const body = document.querySelector('body')
+    const burger = document.querySelector('.burger');
+    const menu = document.querySelector('.menu');
+    const body = document.querySelector('body');
     burger.addEventListener('click', () => {
       if (!menu.classList.contains('active')) {
-        menu.classList.add('active')
-        burger.classList.add('active-burger')
-        body.classList.add('locked')
+        menu.classList.add('active');
+        burger.classList.add('active-burger');
+        body.classList.add('locked');
       } else {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
+        menu.classList.remove('active');
+        burger.classList.remove('active-burger');
+        body.classList.remove('locked');
       }
     })
     // Вот тут мы ставим брейкпоинт навбара
     window.addEventListener('resize', () => {
       if (window.innerWidth > 991.98) {
-        menu.classList.remove('active')
-        burger.classList.remove('active-burger')
-        body.classList.remove('locked')
+        menu.classList.remove('active');
+        burger.classList.remove('active-burger');
+        body.classList.remove('locked');
       }
     })
   }
@@ -32,52 +32,13 @@ function burgerMenu() {
     const nav = document.querySelector('nav')
 
     // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
-    const breakpoint = 1
+    const breakpoint = 1;
     if (window.scrollY >= breakpoint) {
-      nav.classList.add('fixed__nav')
+      nav.classList.add('fixed__nav');
     } else {
-      nav.classList.remove('fixed__nav')
+      nav.classList.remove('fixed__nav');
     }
   }
   window.addEventListener('scroll', fixedNav)
 
 
-
-// // Модальное окно
-// function bindModal(trigger, modal, close) {
-//   trigger = document.querySelector(trigger),
-//     modal = document.querySelector(modal),
-//     close = document.querySelector(close)
-
-//   const body = document.body
-
-//   trigger.addEventListener('click', e => {
-//     e.preventDefault()
-//     modal.style.display = 'flex'
-//     body.classList.add('locked')
-//   });
-//   close.addEventListener('click', () => {
-//     modal.style.display = 'none'
-//      body.classList.remove('locked')
-//   });
-//   modal.addEventListener('click', e => {
-//     if (e.target === modal) {
-//       modal.style.display = 'none'
-//        body.classList.remove('locked')
-//     }
-//   })
-// }
-
-// // ПЕРВЫЙ аргумент - класс кнопки, при клике на которую будет открываться модальное окно.
-// // ВТОРОЙ аргумент - класс самого модального окна.
-// // ТРЕТИЙ аргумент - класс кнопки, при клике на которую будет закрываться модальное окно.
-// bindModal('.modal__btn', '.modal__wrapper', '.modal__close')
-
-
-// // phone mask
-
-// const element = document.querySelector('.phone__input')
-// const maskOptions = {
-//   mask: '+{38}(000)000-00-00'
-// }
-// const mask = IMask(element, maskOptions)
